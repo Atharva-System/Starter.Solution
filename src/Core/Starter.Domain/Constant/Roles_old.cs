8eadeb5b1e6a84d989499aaee72f6a6a407ae080
Tejas Mehta <tejas@atharvasystem.com>
2023-12-06 18:14:36 +0530

    Authorization related code implemetation

---
+++ src/Core/Starter.Domain/Constant/Roles_old.cs
@@ -0,0 +1,22 @@
+﻿namespace Starter.Domain.Constant;
+
+public static class Roles_old
+{
+    public const string Administrator = nameof(Administrator);
+    public const string User = nameof(User);
+
+}
+
+
+public class RolesClaim
+{
+
+    public readonly List<string[]> roleClaims = new List<string[]>
+        {
+            new string[] { Roles_old.Administrator, "TodoItem", "Create" },
+            new string[] { "User", "TodoItem", "Read" },
+            // Add more role claims as needed
+        };
+
+}
+
---
+++ src/InfraStructure/Starter.Identity/Authorization/RoleClaimMapper.cs
@@ -0,0 +1,36 @@
+﻿namespace Starter.Identity.Authorization;
+//public static class RoleClaimMapper
+//{
+//    // ... (other methods)
+
+//    public static string MapResourceToClaim(string resourceName, string permission)
+//    {
+//        // Map resources to claims as needed
+//        return $"{resourceName}.{permission}";
+//    }
+//}
+
+
+public static class RoleClaimMapper
+{
+    public static string MapRoleToClaim(string roleName)
+    {
+        // Map roles to claims as needed
+        switch (roleName)
+        {
+            case Roles.Administrator:
+                return Permissions.Create; // Administrators have create permission
+            case Roles.User:
+                return Permissions.Read; // Users have read permission
+            // Add more role-to-claim mappings as needed
+            default:
+                return string.Empty;
+        }
+    }
+
+    public static string MapResourceToClaim(string resourceName, string permission)
+    {
+        // Map resources to claims as needed
+        return $"{resourceName}.{permission}";
+    }
+}
