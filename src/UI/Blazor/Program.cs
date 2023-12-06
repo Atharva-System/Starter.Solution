563c56a1f2f3521c13235756cef9e717e8ba6659
Rahul Patel <rahul.patel@atharvasystem.com>
2023-12-06 15:37:49 +0530

    Docker Containerize Setup

--- src/UI/Blazor/Program.cs
+++ src/UI/Blazor/Program.cs
@@ -1,4 +1,4 @@
-﻿using Blazor.Starter;
+﻿using Starter.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("AppConfig:ApiUrl");
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(!string.IsNullOrEmpty(apiUrl) ? apiUrl : builder.HostEnvironment.BaseAddress),
+});
 
 await builder.Build().RunAsync();
