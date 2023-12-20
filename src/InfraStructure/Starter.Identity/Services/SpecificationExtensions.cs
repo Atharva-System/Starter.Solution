c041fee6941f5dc45f2ef2dbb1c806dd106761d7
Himang Patel <himang@atharvasystem.com>
2023-12-20 12:18:39 +0530

    Made queryable method of filter specification.

--- src/InfraStructure/Starter.Identity/Services/SpecificationExtensions.cs
+++ src/InfraStructure/Starter.Identity/Services/SpecificationExtensions.cs
@@ -1,25 +1,24 @@
 ﻿using Ardalis.Specification;
 using Ardalis.Specification.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Starter.Identity.Services;
 public static class SpecificationExtensions
 {
-    public static List<T> ApplySpecification<T>(this IEnumerable<T> source, ISpecification<T> specification)
+    public async static Task<List<T>> ApplySpecification<T>(this IQueryable<T> queryable, ISpecification<T> specification)
         where T : class
     {
-        var queryable = source.AsQueryable();
         var specificationEvaluator = new SpecificationEvaluator();
         var result = specificationEvaluator.GetQuery(queryable, specification);
-        return result.ToList<T>();
+        return await result.ToListAsync<T>();
     }
 
-    public static int ApplySpecificationCount<T>(this IEnumerable<T> source, ISpecification<T> specification)
+    public async static Task<int> ApplySpecificationCount<T>(this IQueryable<T> queryable, ISpecification<T> specification)
         where T : class
     {
-        var queryable = source.AsQueryable();
         var specificationEvaluator = new SpecificationEvaluator(new IEvaluator[] { WhereEvaluator.Instance });
         var result = specificationEvaluator.GetQuery(queryable, specification);
-        return result.Count();
+        return await result.CountAsync();
     }
 
 }
--- src/InfraStructure/Starter.Identity/Services/UsersService.cs
+++ src/InfraStructure/Starter.Identity/Services/UsersService.cs
@@ -50,28 +50,29 @@ public partial class UsersService(UserManager<ApplicationUser> userManager,
 
     public async Task<IPagedDataResponse<UserListDto>> SearchAsync(UserListFilter filter, CancellationToken cancellationToken)
     {
-        var usersList = await (from u in _db.Users.AsNoTracking()
-                               join ur in _db.UserRoles.AsNoTracking() on u.Id equals ur.UserId
-                               join r in _db.Roles.AsNoTracking() on ur.RoleId equals r.Id
-                               where u.Id != _currentUserService.UserId
-                               select new UserListDto()
-                               {
-                                   Id = u.Id,
-                                   FirstName = u.FirstName ?? string.Empty,
-                                   LastName = u.LastName ?? string.Empty,
-                                   Email = u.Email ?? string.Empty,
-                                   FullName = u.FirstName + " " + u.LastName,
-                                   Status = u.IsInvitationAccepted == false ? "Invited" : (u.IsActive ? "Active" : "InActive"),
-                                   Role = r.Name ?? string.Empty,
-                                   CreatedOn = u.CreatedOn
-                               }
-                    ).ToListAsync<UserListDto>();
+        var usersList = (from u in _db.Users.AsNoTracking()
+                         join ur in _db.UserRoles.AsNoTracking() on u.Id equals ur.UserId
+                         join r in _db.Roles.AsNoTracking() on ur.RoleId equals r.Id
+                         where u.Id != _currentUserService.UserId
+                         select new UserListDto()
+                         {
+                             Id = u.Id,
+                             FirstName = u.FirstName ?? string.Empty,
+                             LastName = u.LastName ?? string.Empty,
+                             Email = u.Email ?? string.Empty,
+                             FullName = u.FirstName + " " + u.LastName,
+                             Status = u.IsInvitationAccepted == false ? UserStatus.Invited.ToString() : (u.IsActive ? UserStatus.Active.ToString() : UserStatus.InActive.ToString()),
+                             RoleId = r.Id,
+                             Role = r.Name ?? string.Empty,
+                             CreatedOn = u.CreatedOn
+                         }
+                    );
 
         var spec = new GetSearchUserRequestSpec(filter);
 
-        var users = usersList.ApplySpecification(spec);
+        var users = await usersList.ApplySpecification(spec);
 
-        int count = usersList.ApplySpecificationCount(spec);
+        int count = await usersList.ApplySpecificationCount(spec);
 
         return new PagedApiResponse<UserListDto>(count, filter.PageNumber, filter.PageSize) { Data = users };
     }
@@ -111,8 +112,8 @@ public partial class UsersService(UserManager<ApplicationUser> userManager,
             throw new Exception($"Failed to update user: {string.Join(", ", result.Errors.Select(e => e.Description))}");
         }
 
-        return new ApiResponse<string> 
-        { 
+        return new ApiResponse<string>
+        {
             Success = result.Succeeded,
             Data = "User updated successfully.",
             StatusCode = result.Succeeded ? HttpStatusCodes.OK : HttpStatusCodes.BadRequest,
