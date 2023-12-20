a4a46c42e368d7d3dce351ba47380d7c410cff7c
Himang Patel <himang@atharvasystem.com>
2023-12-20 12:18:39 +0530

    Made queryable method of filter specification.

--- src/InfraStructure/Starter.Identity/Services/SpecificationExtensions.cs
+++ src/InfraStructure/Starter.Identity/Services/SpecificationExtensions.cs
@@ -8,7 +8,6 @@ public static class SpecificationExtensions
     public async static Task<List<T>> ApplySpecification<T>(this IQueryable<T> queryable, ISpecification<T> specification)
         where T : class
     {
-        var queryable = source.AsQueryable();
         var specificationEvaluator = new SpecificationEvaluator();
         var result = specificationEvaluator.GetQuery(queryable, specification);
         return await result.ToListAsync<T>();
@@ -17,7 +16,6 @@ public static class SpecificationExtensions
     public async static Task<int> ApplySpecificationCount<T>(this IQueryable<T> queryable, ISpecification<T> specification)
         where T : class
     {
-        var queryable = source.AsQueryable();
         var specificationEvaluator = new SpecificationEvaluator(new IEvaluator[] { WhereEvaluator.Instance });
         var result = specificationEvaluator.GetQuery(queryable, specification);
         return await result.CountAsync();
