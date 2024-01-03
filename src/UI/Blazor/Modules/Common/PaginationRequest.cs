8bc5f33fc3fdbd9d9614cc4759195d112de7cecf
nishatha <nishtha@atharvasystem.com>
2024-01-03 10:51:31 +0530

    Changes in User list blazor

---
+++ src/UI/Blazor/Modules/Common/PaginationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Starter.Blazor.Modules.Common;
+
+public class PaginationRequest
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public string[] OrderBy { get; set; }
+}
---
+++ src/UI/Blazor/Modules/User/Models/InviteUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Starter.Blazor.Modules.User.Models;
+
+public class InviteUserDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
