f59f0ecb32a618cd426ec958f9c5740b8823a646
nishatha <nishtha@atharvasystem.com>
2024-01-05 18:54:02 +0530

    Delete User blazor

--- src/UI/Blazor/Modules/User/Services/IUserService.cs
+++ src/UI/Blazor/Modules/User/Services/IUserService.cs
@@ -12,4 +12,5 @@ public interface IUserService
     Task<ApiResponse<string>> AcceptInvite(UserRegisterDto userRegister);
     Task<string> UpdateUserProfileAsync(string UserId, UpdateProfileDto userDto);
     Task<UpdateProfileDto> GetProfileDetailAsync();
+    Task<ApiResponse<string>> DeleteUser(UserlistDto user);
 }
