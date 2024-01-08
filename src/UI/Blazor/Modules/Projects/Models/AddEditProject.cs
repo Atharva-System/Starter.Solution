520fe637638af8bbcbd2abe96812ce10ad87f006
nishatha <nishtha@atharvasystem.com>
2024-01-08 18:24:58 +0530

    create project blazor

---
+++ src/UI/Blazor/Modules/Projects/Models/AddEditProject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Starter.Blazor.Modules.Projects.Models;
+
+public class AddEditProject
+{
+    public string? Id { get; set; }
+    [Required(ErrorMessage = "Project Name is required")]
+    public string ProjectName { get; set; }
+    public string? Description { get; set; }
+    [Required(ErrorMessage = "Start Date is required")]
+    public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    [Required(ErrorMessage = "End Date is required")]
+    public DateTime EndDate { get; set; } = DateTime.UtcNow;
+    [Required(ErrorMessage = "Estimated Hours is required")]
+    public decimal EstimatedHours { get; set; }
+}
--- src/UI/Blazor/Modules/Projects/Services/ProjectService.cs
+++ src/UI/Blazor/Modules/Projects/Services/ProjectService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using Starter.Blazor.Core.Response;
 using Starter.Blazor.Modules.Common;
 using Starter.Blazor.Modules.Projects.Models;
 using Starter.Blazor.Shared.Response;
@@ -8,6 +9,43 @@ namespace Starter.Blazor.Modules.Projects.Services;
 public class ProjectService(HttpClient http) : IProjectService
 {
     private readonly HttpClient _http = http;
+
+    public async Task<ApiResponse<int>> AddProjectAsync(AddEditProject projectDto)
+    {
+        try
+        {
+            var result = await _http.PostAsJsonAsync("api/Project/Create", projectDto);
+
+            var newResponse = await result.Content.ReadFromJsonAsync<ApiResponse<int>>();
+
+            if (newResponse != null && newResponse.Success)
+            {
+                return newResponse;
+            }
+            else
+            {
+                return new ApiResponse<int>
+                {
+                    Success = false,
+                    Message = newResponse.Message,
+                };
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return new ApiResponse<int>
+            {
+                Success = false,
+            };
+        }
+    }
+
+    public async Task<ApiResponse<ProjectDetailsDto>> GetProjectDetails(string id)
+    {
+        return await _http.GetFromJsonAsync<ApiResponse<ProjectDetailsDto>>($"api/Project/{id}");
+    }
+
     public async Task<List<ProjectDto>> GetProjectlistsAsync(PaginationRequest param)
     {
         try
