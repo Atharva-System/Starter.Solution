e79a4b2a3878731d4661a520d1f746b656e522c3
yashatatharva <yash@atharvasystem.com>
2024-01-16 12:45:48 +0530

    List Rendering

--- src/UI/Blazor/Modules/Task/Model/TaskListDto.cs
+++ src/UI/Blazor/Modules/Task/Model/TaskListDto.cs
@@ -1,14 +1,23 @@
-﻿namespace Starter.Blazor.Modules.Task.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Starter.Blazor.Modules.Task.Model;
 
 public class TaskListDto
 {
     public Guid Id { get; set; }
+    [Required(ErrorMessage = "Task Name is required.")]
     public string TaskName { get; set; }
+    [Required(ErrorMessage = "Description is required.")]
     public string Description { get; set; }
-    public DateTime StartDate { get; set; }
+    [Required(ErrorMessage = "Start Date is required.")]
+    public DateTime StartDate { get; set; } = new DateTime();
+    [Required(ErrorMessage = "End Date is required.")]
     public DateTime EndDate { get; set; }
+    [Required(ErrorMessage = "Status is required.")]
     public int Status { get; set; }
+    [Required(ErrorMessage = "Priority is required.")]
     public int Priority { get; set; }
+    [Required(ErrorMessage = "Project is required.")]
     public Guid ProjectId { get; set; }
     public string AssignedTo { get; set; }
     public DateTime CreatedOn { get; set; }
