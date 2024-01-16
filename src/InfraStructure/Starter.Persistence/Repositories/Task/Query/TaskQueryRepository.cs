cecf3b19a8fab8af222d8072deaa4b21297488c6
Himang Patel <himang@atharvasystem.com>
2024-01-16 17:07:01 +0530

    used async specificaion methods for task search API

--- src/InfraStructure/Starter.Persistence/Repositories/Task/Query/TaskQueryRepository.cs
+++ src/InfraStructure/Starter.Persistence/Repositories/Task/Query/TaskQueryRepository.cs
@@ -38,34 +38,33 @@ public class TaskQueryRepository : QueryRepository<Starter.Domain.Entities.Tasks
 
     public async Task<IPagedDataResponse<TaskListDto>> SearchAsync(ISpecification<TaskListDto> spec, int pageNumber, int pageSize, CancellationToken cancellationToken)
     {
-        var taskList = await (from t in context.Tasks.AsNoTracking()
-                              join p in context.Projects on t.ProjectId equals p.Id
-                              select new TaskListDto
-                              {
-                                  Id = t.Id,
-                                  TaskName = t.TaskName,
-                                  Description = t.Description,
-                                  StartDate = t.StartDate,
-                                  EndDate = t.EndDate,
-                                  StartDateDisplay = CommonFunction.ConvertDateToStringForDisplay(t.StartDate),
-                                  EndDateDisplay = CommonFunction.ConvertDateToStringForDisplay(t.EndDate),
-                                  Priority = t.Priority.ToString(),
-                                  PriorityDisplay = CommonFunction.GetEnumDisplayName((TaskPriority)t.Priority),
-                                  Status = t.Status.ToString(),
-                                  StatusDisplay = CommonFunction.GetEnumDisplayName((TasksStatus)t.Status),
-                                  ProjectId = t.ProjectId,
-                                  AssignedTo = t.AssignedTo,
-                                  CreatedOn = t.CreatedOn.Date,
-                                  CreatedBy = t.CreatedBy,
-                                  ModifiedOn = t.ModifiedOn.DateTime,
-                                  ModifiedBy = t.ModifiedBy,
-                                  ProjectName = p.ProjectName
-                              }).ToListAsync<TaskListDto>();
+        var taskList = from t in context.Tasks.AsNoTracking()
+                       join p in context.Projects on t.ProjectId equals p.Id
+                       select new TaskListDto
+                       {
+                           Id = t.Id,
+                           TaskName = t.TaskName,
+                           Description = t.Description,
+                           StartDate = t.StartDate,
+                           EndDate = t.EndDate,
+                           StartDateDisplay = CommonFunction.ConvertDateToStringForDisplay(t.StartDate),
+                           EndDateDisplay = CommonFunction.ConvertDateToStringForDisplay(t.EndDate),
+                           Priority = t.Priority.ToString(),
+                           PriorityDisplay = CommonFunction.GetEnumDisplayName((TaskPriority)t.Priority),
+                           Status = t.Status.ToString(),
+                           StatusDisplay = CommonFunction.GetEnumDisplayName((TasksStatus)t.Status),
+                           ProjectId = t.ProjectId,
+                           AssignedTo = t.AssignedTo,
+                           CreatedOn = t.CreatedOn.Date,
+                           CreatedBy = t.CreatedBy,
+                           ModifiedOn = t.ModifiedOn.DateTime,
+                           ModifiedBy = t.ModifiedBy,
+                           ProjectName = p.ProjectName
+                       };
 
+        var tasks = await taskList.ApplySpecification(spec);
 
-        var tasks = taskList.ApplySpecification(spec);
-
-        var count = taskList.ApplySpecificationToListCount(spec);
+        var count = await taskList.ApplySpecificationCount(spec);
 
         return new PagedApiResponse<TaskListDto>(count, pageNumber, pageSize) { Data = tasks };
     }
