f519df7a1de9f377158ced1c0c551ac22036cfcb
yashatatharva <yash@atharvasystem.com>
2024-01-17 11:54:30 +0530

    Start Date Validation added

---
+++ src/UI/Blazor/Modules/Common/DateInFutureAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Starter.Blazor.Modules.Common;
+
+public class DateInFutureAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is DateTime dateValue)
+        {
+            if (dateValue.Date < DateTime.Now.Date)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
